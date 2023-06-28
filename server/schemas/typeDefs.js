d486259b841055475ace4ddb39db201d39d10c1d
lillianlaihughes <lillianlaihughes@gmail.com>
2023-06-28 11:19:36 -0400

    updated typeDefs with more pseudo code

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,6 +20,27 @@ type Filament {
 type Printable {
 
 }
+
+type Auth {
+  token: ID
+  user: User
+  }
+
+type Query {
+  user: User
+                            /*
+                            ADD OTHER QUERIES HERE
+                            */
+
+}
+
+type Mutation {
+  addUser()
+  login()
+                            /*
+                            ADD MUTATIONS HERE - add, update, delete, etc.
+                            */
+}
 `;
 
 // export module so it can be used
