eee15ab6adf644a4c90f9d95f2261b8a9f449f07
Raiya Young <121911949+bundleofcodes@users.noreply.github.com>
2023-06-28 17:11:00 -0400

    added starter code to login.js and register.js files

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,32 @@
+
 import logo from './logo.svg';
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import './App.css';
-import navbar from "./components/navbar/Navbar";
-import login from "./components/login/Login";
-import signup from "./components/signup/Signup";
-import footer from "./components/footer/Footer";
-import logout from "./components/logout/Logout";
-import userProfile from "./components/UserProfile";
-import sideBar from "./components/sideBar/SideBar";
-import pageList from "./components/pageList/PageList"
+import { Login } from "./components/login/Login";
+
+import { Signup } from "./components/signup/Signup";
+
+// import navbar from "./components/navbar/Navbar";
+// import login from "./components/login/Login";
+// import signup from "./components/signup/Signup";
+// import footer from "./components/footer/Footer";
+// import logout from "./components/logout/Logout";
+// import userProfile from "./components/UserProfile";
+// import sideBar from "./components/sideBar/SideBar";
+// import pageList from "./components/pageList/PageList"
 
 function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Login />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
