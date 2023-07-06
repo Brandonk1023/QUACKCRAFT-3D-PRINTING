5325c4503bc3edef984700682894c007def0cd70
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-06 14:53:05 -0400

    added react router routes into app.js, added react router to all package.json files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-
+import React from "react";
 import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import { Login } from "./components/login/Login";
 import { Register } from "./components/register/Register";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // import navbar from "./components/navbar/Navbar";
 // import footer from "./components/footer/Footer";
@@ -20,12 +21,18 @@ function App() {
   }
   
   return (
+    <Router>
     <div className="App">
+      <Routes>
+        <Route exact path="/" element={<home />}/>
+        <Route path="/login" element={<login />}/>
+      </Routesoutes>
       {
         currentForm === "Login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
       }
      {/* <Login /> */}
     </div>
+    <Router/>
   );
 }
 
