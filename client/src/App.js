35a13107220651988dbecb410937ffd7ade3acb3
lillianlaihughes <lillianlaihughes@gmail.com>
2023-07-06 16:58:52 -0400

    revised typos client app.js (uppercase vs lower)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-
-import React, { useState } from "react";
-import logo from './logo.svg';
-import './App.css';
-import Navbar from "./components/navbar/Navbar";
-import { Login } from "./components/login/Login";
-import { Register } from "./components/register/Register";
-import { AccountSettings } from "./components/userProfile/AccountSettings";
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Navbar from './components/navbar/navbar'
+import { Login } from './components/login/login'
+import { Register } from './components/register/Register'
+import { AccountSettings } from './components/userProfile/AccountSettings'
 // imports for userProfile
 import SimpleBar from 'simplebar-react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
@@ -14,12 +13,11 @@ import { theme } from './components/userProfile/helpers/index'
 import Cover from './components/userProfile/Cover'
 import Main from './components/userProfile/Main'
 
-
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
+  const [currentForm, setCurrentForm] = useState('login')
 
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
+  const toggleForm = formName => {
+    setCurrentForm(formName)
   }
 
   return (
@@ -29,20 +27,19 @@ function App() {
       } */}
       {/* <Login /> */}
 
-      <SimpleBar style={{ maxHeight: '100vh'}}>
+      <SimpleBar style={{ maxHeight: '100vh' }}>
         <ChakraProvider theme={theme}>
           <Router>
             <Navbar />
             <Switch>
-              <Route path= "/">
+              <Route path="/">
                 <Login />
               </Route>
-              <Route path= "/register">
+              <Route path="/register">
                 <Register />
               </Route>
-              <Route path= "/cover">
-              </Route>
-              <Route path= "/main">
+              <Route path="/cover"></Route>
+              <Route path="/main">
                 <Cover />
                 <Main />
               </Route>
@@ -50,15 +47,8 @@ function App() {
           </Router>
         </ChakraProvider>
       </SimpleBar>
-
     </div>
-  );
+  )
 }
 
-
-export default App;
-
-
-
-
-
+export default App
