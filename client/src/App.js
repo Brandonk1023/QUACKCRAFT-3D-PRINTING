5c2f266c5c20199733ad6f5a1b41f1bcbadba253
lillianlaihughes <lillianlaihughes@gmail.com>
2023-07-06 21:34:06 -0400

    edited App.js to clear out problems

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,31 @@
-
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Navbar from './components/navbar/navbar'
-import { Login } from './components/login/login'
-import { Register } from './components/register/Register'
-import { AccountSettings } from './components/userProfile/AccountSettings'
-import React from "react";
 import React, { useState } from "react";
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Navbar from "./components/navbar/Navbar";
-import { Login } from "./components/login/Login";
+import Navbar from "./components/navbar/navbar";
+import { Login } from "./components/login/login";
 import { Register } from "./components/register/Register";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AccountSettings } from "./components/userProfile/AccountSettings";
 // imports for userProfile
-import SimpleBar from 'simplebar-react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from './components/userProfile/helpers/index'
-import Cover from './components/userProfile/Cover'
-import Main from './components/userProfile/Main'
-
+// import { AccountSettings } from "./components/userProfile/AccountSettings";
+import SimpleBar from 'simplebar-react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './components/userProfile/helpers/index';
+// import Cover from './components/userProfile/Cover';
+// import Main from './components/userProfile/Main';
+// RAIYA and ETHAN -- need to review where the above imports for logo, AccountSetting, Cover, Main are be used
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login')
-
+// RAIYA and ETHAN -- need to review where `currentForm` is used
   const toggleForm = formName => {
     setCurrentForm(formName)
   }
 
   return (
-    <Router>
     <div className="App">
 
         currentForm === "Login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      } */}
-      {/* <Login /> */}
-
+      
     <SimpleBar style={{ maxHeight: '100vh' }}>
       <ChakraProvider theme={theme}>
         <Router>
@@ -61,5 +48,7 @@ function App() {
       </ChakraProvider>
     </SimpleBar>
     </div>
+  );
+  }
 
-export default App
+export default App;
