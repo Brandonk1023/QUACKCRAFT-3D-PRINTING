14921456c7cd5de8355d9350cc30529eab650c14
Raiya Young <121911949+bundleofcodes@users.noreply.github.com>
2023-07-10 12:20:47 -0400

    finalizing merge for main from raiya-react branch

--- client/src/App.js
+++ client/src/App.js
@@ -46,36 +46,6 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 })
 
-=======
-// import Cover from './components/userProfile/Cover';
-// import Main from './components/userProfile/Main';
-// RAIYA and ETHAN -- need to review where the above imports for logo, AccountSetting, Cover, Main are be used
->>>>>>> 8036bad43298670cdcc8708022793e54e731e445
-
-// Construct our main GraphQL API endpoint
-const httpLink = createHttpLink({
-  uri: '/graphql',
-})
-
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token')
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  }
-})
-
-const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-})
-
 function App() {
   const [currentForm, setCurrentForm] = useState('login')
   // RAIYA and ETHAN -- need to review where `currentForm` is used
@@ -104,6 +74,7 @@ function App() {
                   <Route path="/register" />
                   <Route path="/cover" />
                   <Route path="/main" />
+                  
                 </Routes>
               </Navbar>
             </Router>
