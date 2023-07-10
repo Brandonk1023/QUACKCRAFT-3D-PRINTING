ae6a8d8ccb721aa27e8ada925ab92178f53ed61e
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-10 16:03:17 -0400

    downloables pages and filament library pages into structure with beginning code

---
+++ client/src/components/downloables/downloadables.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Downloadables = () => {
+    return (
+        <div className='App'>
+            <h1>Testing</h1>
+
+        </div>
+    )
+}
+
+export default Downloadables;
