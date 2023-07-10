09badd7810cea0679cc8bd33ce6b3b518d1799e1
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-10 15:18:32 -0400

    added new schema for downloadables

--- server/models/PrintableFile.js
+++ server/models/PrintableFile.js
@@ -8,3 +8,25 @@ OUR OWN SEEDED SERVER?
 need to think more
 
 */
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const downloadablesSchema = new Schema({
+    Title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    Content: {
+        type: string,
+        required: true,
+        trim: true,
+    },
+    DownloadURL: {
+        type: string,
+        required: true,
+    },
+})
+
+module.exports = Downloables
