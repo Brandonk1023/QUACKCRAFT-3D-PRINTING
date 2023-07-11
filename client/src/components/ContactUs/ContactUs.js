6226641075057a374a1c45676d71866c0b63da19
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-11 00:54:02 -0400

    added created contactus page and added basic coding

---
+++ client/src/components/ContactUs/ContactUs.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ContactUs = () => {
+    return(
+        <section>
+            <div>
+                <h2>Contact Us</h2>
+                <p>Are you passionate about 3D printing as much as we are?  Would you like to contribute?  If so please feel free to reach out to our team by clicking on the email link below.  We will be in touch with you as opportunities become available.  We look foward to chatting with you soon.</p>
+                <a href='mailto:contributors@quackcraft.com'>Email</a>
+            </div>
+        </section>
+    )
+}
+
+export default ContactUs;
+
