cfca65a2878c401ed9b0b86d537f6b4cd68b132d
lillianlaihughes <lillianlaihughes@gmail.com>
2023-07-10 20:45:05 -0400

    revised class to className where found

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,47 +1,67 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 export default function Login(props) {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
 
-    // start code: not sure if I need this
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  // start code: not sure if I need this
+  const handleEmailChange = e => {
+    setEmail(e.target.value)
+  }
 
-    const PasswordChange = (e) => {
-        setPass(e.target.value);
-    };
-    // end code
+  const PasswordChange = e => {
+    setPass(e.target.value)
+  }
+  // end code
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
-        // not sure if this correct: login logic for making API call
-        // const loginData = {
-        //     email: email,
-        //     pass: pass
-        // };
-        // makeLoginAPIRequest(loginData);
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(email)
+    // not sure if this correct: login logic for making API call
+    // const loginData = {
+    //     email: email,
+    //     pass: pass
+    // };
+    // makeLoginAPIRequest(loginData);
 
-        // Reset form fields after successful login
-        setEmail('');
-        setPass('');
-    }
+    // Reset form fields after successful login
+    setEmail('')
+    setPass('')
+  }
 
-
-    return (
-        // <>Login </>
-        <div className="auth-form-container">
-            <h2 className="header-text">Welcome Back!</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label for="email">Email</label>
-                <input class="input-box" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="enter your email" id="email" name="email" />
-                <label for="password">Password</label>
-                <input class="input-box" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button className="hover">Log in!</button>
-            </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('Register')}>Need an account? Register here!</button>
-        </div>
-    );
-};
+  return (
+    // <>Login </>
+    <div className="auth-form-container">
+      <h2 className="header-text">Welcome Back!</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label for="email">Email</label>
+        <input
+          className="input-box"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          type="email"
+          placeholder="enter your email"
+          id="email"
+          name="email"
+        />
+        <label for="password">Password</label>
+        <input
+          className="input-box"
+          value={pass}
+          onChange={e => setPass(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+        />
+        <button className="hover">Log in!</button>
+      </form>
+      <button
+        className="link-btn"
+        onClick={() => props.onFormSwitch('Register')}
+      >
+        Need an account? Register here!
+      </button>
+    </div>
+  )
+}
--- client/src/components/logout/Logout.js
+++ client/src/components/logout/Logout.js
@@ -1,49 +1,69 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-export const Logout = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+export const Logout = props => {
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
 
-    // start code: not sure if I need this
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  // start code: not sure if I need this
+  const handleEmailChange = e => {
+    setEmail(e.target.value)
+  }
 
-    const PasswordChange = (e) => {
-        setPass(e.target.value);
-    };
-    // end code
+  const PasswordChange = e => {
+    setPass(e.target.value)
+  }
+  // end code
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
- // not sure if this correct: login logic for making API call
-        // const loginData = {
-        //     email: email,
-        //     pass: pass
-        // };
-        // makeLoginAPIRequest(loginData);
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(email)
+    // not sure if this correct: login logic for making API call
+    // const loginData = {
+    //     email: email,
+    //     pass: pass
+    // };
+    // makeLoginAPIRequest(loginData);
 
-        // Reset form fields after successful login
-        setEmail('');
-        setPass('');
-    }
+    // Reset form fields after successful login
+    setEmail('')
+    setPass('')
+  }
 
+  return (
+    // <>Logout </>
+    <div className="auth-form-container">
+      <h2 className="header-text">We look forward to seeing you again!</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label for="email">Email</label>
+        <input
+          className="input-box"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          type="email"
+          placeholder="enter your email"
+          id="email"
+          name="email"
+        />
+        <label for="password">Password</label>
+        <input
+          className="input-box"
+          value={pass}
+          onChange={e => setPass(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+        />
+        <button className="hover">Log in!</button>
+      </form>
+      <button
+        className="link-btn"
+        onClick={() => props.onFormSwitch('Register')}
+      >
+        Need an account? Register here!
+      </button>
+    </div>
+  )
+}
 
-    return (
-        // <>Logout </>
-        <div className="auth-form-container">
-            <h2 className="header-text">We look forward to seeing you again!</h2>
-        <form className="login-form" onSubmit={handleSubmit}>
-            <label for="email">Email</label>
-            <input class="input-box" value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="enter your email" id="email" name="email" />
-            <label for="password">Password</label>
-            <input class="input-box" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button className="hover">Log in!</button>
-        </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('Register')}>Need an account? Register here!</button>
-        </div>
-    );
-};
-
-export default Logout;
+export default Logout
