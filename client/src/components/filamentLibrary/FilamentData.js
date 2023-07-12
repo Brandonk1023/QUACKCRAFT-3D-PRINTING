fc7943a16e6f672a8d0b714691ffe61b668f1167
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-12 12:09:48 -0400

    added button components for links for navbar

--- client/src/components/filamentLibrary/FilamentData.js
+++ client/src/components/filamentLibrary/FilamentData.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+
 
 const FilamentData = ({ filamentData }) => {
     return (
-        <Card sx={{ maxWidth: 600 }}>
+        <Card sx={{ maxWidth: 400 }}>
             <CardMedia>
-            <img height="375" src={filamentData.image} alt={filamentData.filamentType} />
+            <img height="375" width="375" src={filamentData.image} alt={filamentData.filamentType} />
             </CardMedia>
             <CardContent>
             <h3>{filamentData.filamentType}</h3>
--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,35 +1,49 @@
 import React from 'react'
 import './navbar.scss'
 import { Link } from 'react-router-dom'
+import { Button } from '@mui/material'
 
 function Navbar() {
-  // RAIYA'S CODE for navbar styling (we can revert back to Ethan's code if need to)
   return (
     <nav className="navbar">
       <a href="#about" className="brandlogo">
         QuackCraft 3D Printing
       </a>
       <ul>
-        <li className="active">
+        <li>
+        <Button variant="contained">
         <Link to="/about">About Us</Link>
+        </Button>
         </li>
         <li>
+        <Button variant="contained">
         <Link to="/contact">Contact</Link>
+        </Button>
         </li>
         <li>
+        <Button variant="contained">
         <Link to="/downloadables">Downloadables</Link>
+        </Button>
         </li>
         <li>
+        <Button variant="contained">
         <Link to="/tutorials">Tutorials</Link>
+        </Button>
         </li>
         <li>
+        <Button variant="contained">
         <Link to="/filaments">Filament Library</Link>
+        </Button>
         </li>
         <li>
+        <Button variant="contained">
         <Link to="/donate">Donate</Link>
+        </Button>
         </li>
         <li>
+        <Button variant="contained">
         <Link to="/login">Login</Link>
+        </Button>
         </li>
       </ul>
     </nav>
