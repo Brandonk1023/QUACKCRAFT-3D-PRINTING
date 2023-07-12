e43d77ce8e2d352d2c6e9efd4cf88aafc3423f4b
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-12 08:27:31 -0400

    added auth to utils in server

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,5 +1,31 @@
-// this file is required in `../schemas/resolvers.js`
+const jwt = require('jsonwebtoken');
 
-// work on this later (per CJ's recommendation)
+const secret = 'Supersecretpasswordcookie';
+const expiration = '4h';
 
-// This is where we use JSONwebtokens
+module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
