54996d200feb8b8b03626283de6c80393b79f084
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-12 11:50:13 -0400

    added card component filament library page

--- client/src/components/filamentLibrary/FilamentData.js
+++ client/src/components/filamentLibrary/FilamentData.js
@@ -1,16 +1,26 @@
 import React from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const FilamentData = ({ filamentData }) => {
     return (
-        <div>
-            <img src={filamentData.image} alt={filamentData.filamentType} />
+        <Card sx={{ maxWidth: 600 }}>
+            <CardMedia>
+            <img height="375" src={filamentData.image} alt={filamentData.filamentType} />
+            </CardMedia>
+            <CardContent>
             <h3>{filamentData.filamentType}</h3>
             <ul>
                 <li>Printer Temp: {filamentData.printTemp}</li>
                 <li>Bed Temp: {filamentData.bedTemp}</li>
                 <li>Print Speed: {filamentData.printSpeed}</li>
             </ul>
-        </div>
+            </CardContent>
+        </Card>
     )
 }
 
