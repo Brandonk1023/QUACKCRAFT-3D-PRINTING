e790770448c7f96e1c55a7bb8e4c945befdbadef
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-13 00:57:24 -0400

    removed mongoose local call

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 // create connection
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/quackcraft-3D-printing',
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
