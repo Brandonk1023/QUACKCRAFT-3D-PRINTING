4f7d7d022c35b0badd611139e7934f152827c7b8
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-13 01:19:08 -0400

    commented out mongoose connection so we can get through deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,14 +2,14 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// create connection
+/* // create connection
 mongoose.connect(
   process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
-);
+); */
 
 // export connection
 module.exports = mongoose.connection;
